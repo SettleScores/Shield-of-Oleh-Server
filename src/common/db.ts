551cef9c9245874e6bc46725483bad99e1f1bb77
SettleScores <settle.scores.biz@gmail.com>
2026-04-15 11:36:28 +0300

    Initial commit Master of puppies

---
+++ src/common/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    const dbUri = process.env.MONGO_URI;
+
+    console.log('qqq__db.ts_connectDB dbUri: ' + dbUri);
+
+    if (!dbUri)
+        throw new Error('Mongo URI is not defined!');
+
+    try {
+        await mongoose.connect(dbUri);
+
+        console.log('MongoDB connected!');
+    }
+    catch (err) {
+        console.error('Mongo error: ', err);
+
+        process.exit(1);
+    }
+}
